dfdf3af354095197f3b9b566f58285e88d8aefe2
Bagas Wahyu Hidayah <bagasdotid69@gmail.com>
2019-12-31 20:39:09 +0700

    refactor

--- parser.go
+++ parser.go
@@ -2,7 +2,10 @@ package telebparser
 
 import (
 	"io"
+	"io/ioutil"
 	"log"
+	"os"
+	"path"
 	"regexp"
 	"strconv"
 	"time"
@@ -10,21 +13,40 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
+// MessageRoom represents one chat room.
 type MessageRoom struct {
+	// The name of the room.
 	RoomName string
+
+	// List of messages in the room.
 	Messages []Message
 }
 
+// Message represents one individual message sent by user.
 type Message struct {
-	ID        string
-	DateSent  time.Time
-	FromName  string
+	// ID is unique for each message.
+	// It is assigned in `id` attribute.
+	ID string
+
+	// When the message is sent.
+	DateSent time.Time
+
+	// The name of the user who sends the message.
+	SenderName string
+
+	// If this message is replying other message, this is the ID this message is replying to.
+	// Empty string if not replying.
 	ReplyToID string
-	Content   string
+
+	// Content is the message content.
+	// This data could be anything: text, media, audio, etc.
+	Content interface{}
 }
 
+// Regular expression for date string on `.date`'s title attribute inside `.body`.
 var dateRe = regexp.MustCompile("(\\d{2})\\.(\\d{2})\\.(\\d{4}) (\\d{2}):(\\d{2}):(\\d{2})")
 
+// getTimeComponents extracts individual "date components" (day, month, year, etc.) from the date string.
 func getTimeComponents(dateString string) (day, month, year, hour, minute, second int) {
 	dateComponents := dateRe.FindSubmatch([]byte(dateString))
 	day, _ = strconv.Atoi(string(dateComponents[1]))
@@ -37,6 +59,7 @@ func getTimeComponents(dateString string) (day, month, year, hour, minute, secon
 	return
 }
 
+// getTimeValue creates time.Time value from extracted date components.
 func getTimeValue(day, month, year, hour, minute, second int, locString string) (time.Time, error) {
 	timeLocation, err := time.LoadLocation(locString)
 	if err != nil {
@@ -55,6 +78,7 @@ func parseTime(dateString string, locString string) (time.Time, error) {
 	return timeValue, nil
 }
 
+// parseMessage parses individual `.message` element.
 func parseMessage(s *goquery.Selection, prevFromName *string) Message {
 	if !s.HasClass("default") {
 		return Message{}
@@ -65,6 +89,8 @@ func parseMessage(s *goquery.Selection, prevFromName *string) Message {
 	ID, _ := s.Attr("id")
 
 	body := s.Find(".body")
+
+	// Message inside `div.message` that has `joined` class is sent by previous message's sender (recursively).
 	if s.HasClass("joined") {
 		fromName = *prevFromName
 	} else {
@@ -79,23 +105,70 @@ func parseMessage(s *goquery.Selection, prevFromName *string) Message {
 		return Message{}
 	}
 
-	content := body.Find(".text").Text()
+	// content parsing is tricky
+	var content interface{}
+	content = body.Find(".text").Text()
 
-	message := Message{ID, dateSent, fromName, content}
-
-	return message
+	return Message{ID, dateSent, fromName, "", content}
 }
 
-func Parse(f io.Reader, messages *[]Message) error {
-	doc, err := goquery.NewDocumentFromReader(f)
-	if err != nil {
-		return err
-	}
-
+// parseFile parses an html file.
+func parseFile(doc *goquery.Document, messages *[]Message) error {
 	var prevFromName string
 	doc.Find(".message").Each(func(i int, s *goquery.Selection) {
 		message := parseMessage(s, &prevFromName)
 		*messages = append(*messages, message)
+	})
+
+	return nil
+}
+
+func forEachFile(root string, fn func(r io.Reader) error) error {
+	dirs, err := ioutil.ReadDir(root)
+	if err != nil {
+		return err
+	}
+
+	re := regexp.MustCompile("^messages\\d*\\.html")
+	for i := range dirs {
+		if dirs[i].IsDir() {
+			continue
+		}
+
+		filename := dirs[i].Name()
+		fullpath := path.Join(root, filename)
+		if re.Match([]byte(filename)) {
+			f, err := os.Open(fullpath)
+			if err != nil {
+				return err
+			}
+
+			fn(f)
+
+			f.Close()
+		}
+	}
+
+	return nil
+}
+
+func Parse(root string, messageRoom *MessageRoom) error {
+	hasGotRoomName := false
+	forEachFile(root, func(r io.Reader) error {
+		doc, err := goquery.NewDocumentFromReader(r)
+		if err != nil {
+			return err
+		}
+
+		if !hasGotRoomName {
+			// Parse room name.
+			messageRoom.RoomName = doc.Find(".page_header").Find(".text").Text()
+			hasGotRoomName = true
+		}
+
+		parseFile(doc, &messageRoom.Messages)
+
+		return nil
 	})
 
 	return nil
--- parser_test.go
+++ parser_test.go
@@ -1,43 +1,16 @@
 package telebparser
 
 import (
-	"io/ioutil"
-	"os"
-	"path"
-	"regexp"
+	"fmt"
 	"testing"
 )
 
 func parse() error {
 	filepath := "C:\\Users\\Bagas Wahyu Hidayah\\Downloads\\Telegram Desktop\\ChatExport_30_12_2019"
-	dirs, err := ioutil.ReadDir(filepath)
-	if err != nil {
-		return err
-	}
 
-	messages := make([]Message, 0, 100)
-
-	re := regexp.MustCompile("^messages\\d*\\.html")
-	for i := range dirs {
-		if dirs[i].IsDir() {
-			continue
-		}
-
-		filename := dirs[i].Name()
-		fullpath := path.Join(filepath, filename)
-		if re.Match([]byte(filename)) {
-			f, err := os.Open(fullpath)
-			if err != nil {
-				return err
-			}
-
-			Parse(f, &messages)
-			// fmt.Println(messages)
-
-			f.Close()
-		}
-
-	}
+	var messageRoom MessageRoom
+	Parse(filepath, &messageRoom)
+	fmt.Println(messageRoom)
 
 	return nil
 }
